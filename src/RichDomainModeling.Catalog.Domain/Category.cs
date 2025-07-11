4ee2243fa3ae48e19ee1b6b999a7446688ffbd63
Mateus Nadal <nadalmateus@hotmail.com>
2025-07-10 22:26:18 -0300

    Add constructor and update methods to Category class
    - Implemented a constructor to initialize Name and Code properties.
    - Added UpdateName and UpdateCode methods for property modification.
    - Overrode ToString method for formatted string representation.

--- src/RichDomainModeling.Catalog.Domain/Category.cs
+++ src/RichDomainModeling.Catalog.Domain/Category.cs
@@ -4,5 +4,27 @@ namespace RichDomainModeling.Catalog.Domain;
 
 public class Category : Entity
 {
+    public Category(string name, int code)
+    {
+        Name = name;
+        Code = code;
+    }
+
     public string Name { get; private set; }
+    public int Code { get; private set; }
+
+    public void UpdateName(string name)
+    {
+        Name = name;
+    }
+
+    public void UpdateCode(int code)
+    {
+        Code = code;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} - {Code}";
+    }
 }
